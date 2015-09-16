5bdecf9f2d5d345ad7b76b782c4cc94b730df36c
EwanC <ewan.cr@gmail.com>
2015-09-16 20:57:40 +0100

    Rename hello.asm to boot.asm

--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
 Project to create own OS.
 
 Following guide: http://www.cs.bham.ac.uk/~exr/lectures/opsys/10_11/lectures/os-dev.pdf
+and using MikeOS as a reference
--- go.sh
+++ go.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Compile
-nasm -f bin -o hello.bin hello.asm
+nasm -f bin -o boot.bin boot.asm
 
 # Boot from floppy
-qemu-system-x86_64 -boot order=a -fda hello.bin
+qemu-system-x86_64 -boot order=a -fda boot.bin
